ec0634152b932a4fa06d60d89b564dff93d3a139
cyau60379 <cyril.aubourg@isep.fr>
2020-03-31 15:51:19 +0200

    python server added

--- web_service/src/main/java/com/cthulhu/web_service/WebServiceApplication.java
+++ web_service/src/main/java/com/cthulhu/web_service/WebServiceApplication.java
@@ -1,13 +1,20 @@
 package com.cthulhu.web_service;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WebServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebServiceApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+    }
 }
