318ee8c5c2b7ca18e1996a4d6a79db44c04eee60
cyau60379 <cyril.aubourg@isep.fr>
2020-04-13 12:26:50 +0200

    configure URLs, fill models.py with DB structure

--- web_service_python/web_service/models.py
+++ web_service_python/web_service/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Affiliation(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Creature(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    image_link = models.CharField(max_length=200)
+    affiliation = models.ForeignKey(Affiliation, on_delete=models.CASCADE)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    birth_date = models.DateField()
+    death_date = models.DateField()
+    image_link = models.CharField(max_length=200)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    year_of_creation = models.DateField()
+    image_link = models.CharField(max_length=200)
+
+
+class CreatureBook(models.Model):
+    creature = models.ForeignKey(Creature, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+
+class AuthorBook(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+
+class AuthorCreature(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    creature = models.ForeignKey(Creature, on_delete=models.CASCADE)
