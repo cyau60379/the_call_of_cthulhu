79a64926e64b214d90652a91bcbfb8ab613f7140
cyau60379 <cyril.aubourg@isep.fr>
2020-04-26 15:46:11 +0200

    JAVA: KeyLoader Class creation

---
+++ web_service/src/main/java/com/cthulhu/web_service/KeyLoader.java
@@ -0,0 +1,141 @@
+package com.cthulhu.web_service;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+/**
+ * 
+ * @author Cyril AUBOURG
+ * Class which loads the private and the public key contained in the resources
+ *
+ */
+public class KeyLoader {
+	
+	/**
+	 * Function which initialize the key factory
+	 * @param algorithm: algorithm of the key
+	 * @return the key factory
+	 */
+	private static KeyFactory getFactory(String algorithm) {
+		try {
+			KeyFactory factory = KeyFactory.getInstance(algorithm);    //initialize the factory on RSA algorithm
+			return factory;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/**
+	 * Function which transform data in byte tab
+	 * @param fileLocation: the location of the private key
+	 * @return the byte table of the file's data
+	 */
+	private static byte[] prepareData(String fileLocation) {
+		
+		/******************** Variables **********************/
+		File file = new File(fileLocation);   //load the file
+		FileInputStream fileStream;    //input bytes from the file
+		DataInputStream dataStream;    //read Java primitive data types (here input bytes from fileStream)
+		byte[] byteData;    //bytes tab from the data file
+		
+		try {
+			fileStream = new FileInputStream(file);
+			dataStream = new DataInputStream(fileStream);
+	        byteData = new byte[(int)file.length()];
+	        try {    //fill with the bytes from the fileStream
+				dataStream.read(byteData);
+				dataStream.close();
+				return byteData;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Error: File not found, PLEASE verify that the location " + fileLocation + " exists.");
+			return null;
+		}
+	}
+	
+	/**
+	 * Function which takes the private key of this server and convert it for Java use
+	 * @param fileLocation: the location of the private key
+	 * @return the private key
+	 */
+	public static PrivateKey getPrivateKey(String fileLocation) {
+		
+		/******************** Variables **********************/
+		byte[] privateKeyB;    //bytes tab from the private key
+		KeyFactory factory;    //factory which will generate the key from the decoded file
+		PKCS8EncodedKeySpec privateSpecification;   //specification on the encoded method of the input file
+		PrivateKey privateKey;    //the private key which will be returned
+		
+		factory = KeyLoader.getFactory("RSA");
+		privateKeyB = KeyLoader.prepareData(fileLocation);
+		if(privateKeyB == null) {
+			return null;
+		}
+        privateSpecification = new PKCS8EncodedKeySpec(privateKeyB);  //initialize the specification of the key (PKCS8 here)
+        
+		try {
+			privateKey = factory.generatePrivate(privateSpecification);  //generate the new key
+		} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+			privateKey = null;
+		}
+		return privateKey;   //return the private key
+	}
+	
+	/**
+	 * Function which takes the public key of the other server and convert it for Java use
+	 * @param fileLocation: the location of the public key
+	 * @return the public key
+	 */
+	public static PublicKey getPublicKey(String fileLocation) {
+		
+		/******************** Variables **********************/
+		byte[] publicKeyB;    //bytes tab from the public key
+		KeyFactory factory;    //factory which will generate the key from the decoded file
+		X509EncodedKeySpec publicSpecification;   //specification on the encoded method of the input file
+		PublicKey publicKey;    //the public key which will be returned
+		
+		factory = KeyLoader.getFactory("RSA");
+		publicKeyB = KeyLoader.prepareData(fileLocation);
+		if(publicKeyB == null) {
+			return null;
+		}
+        publicSpecification = new X509EncodedKeySpec(publicKeyB);  //initialize the specification of the key (PKCS8 here)
+        
+		try {
+			publicKey = factory.generatePublic(publicSpecification);  //generate the new key
+		} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+			publicKey = null;
+		}
+		return publicKey;   //return the public key
+	}
+	
+	/**
+	public static void main(String[] args) {
+		byte[] pk = KeyLoader.getPrivateKey("src\\main\\resources\\static\\privateKeySpring.der").getEncoded();
+        for(int i = 0; i < pk.length; i++) {
+        	System.out.print(pk[i]);
+        }
+        System.out.println();
+		byte[] pubk = KeyLoader.getPublicKey("src\\main\\resources\\static\\publicKeyDjango.der").getEncoded();
+        for(int i = 0; i < pubk.length; i++) {
+        	System.out.print(pubk[i]);
+        }
+	}
+	**/
+}
--- web_service/src/main/java/com/cthulhu/web_service/WebServiceApplication.java
+++ web_service/src/main/java/com/cthulhu/web_service/WebServiceApplication.java
@@ -1,9 +1,18 @@
 package com.cthulhu.web_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
@@ -17,4 +26,39 @@ public class WebServiceApplication {
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
     }
+	
+	@RequestMapping( value= "/controller3", method = RequestMethod.POST)
+	 public @ResponseBody void process(@RequestBody String jsonString){
+
+	    //retrieve jsonString and transform it to retrieve all data
+		String request = "requestType"; //Get the request type from request
+	    String url = "";
+	    MultiValueMap<String, String> params= null;
+	    switch(request){
+	         case "request1": //how to call REST API 1?
+	            url = "/controller1";
+	            params = ; //Get the parameter map from request 
+	         case "request2": //how to call REST API 2?
+	            url = "/controller2";
+	            params = request2Param; //Get the parameter map from request 
+	      }
+
+	      //Now call the method with parameters
+	      getRESTResponse(url, params);
+
+	  }
+
+	  private String getRESTResponse(String url, MultiValueMap<String, String> params){
+	     RestTemplate template = new RestTemplate();
+	     HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(params);
+	    String response = "";
+	     try{
+	        ResponseEntity<String> responseEntity = template.exchange(url, HttpMethod.POST, requestEntity,  String.class);
+	        response = responseEntity.getBody();
+	    }
+	    catch(Exception e){
+	        response = e.getMessage();
+	    }
+	    return response;
+	  }
 }
